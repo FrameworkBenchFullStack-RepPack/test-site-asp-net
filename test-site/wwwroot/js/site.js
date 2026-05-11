673738e27a8ee175b1919e8213f43a8a946181b7
Anton <dev@atjn.dk>
2026-05-11 12:56:25 +0200

    Use HTMX boost, fix script module loading

--- test-site/wwwroot/js/site.js
+++ test-site/wwwroot/js/site.js
@@ -1,13 +1,12 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("filter-form");
-  if (!form) return;
+import "htmx.org";
+import "htmx-ext-head-support";
 
-  form.addEventListener("htmx:afterRequest", () => {
-    const url = new URL(window.location.href);
-    url.search = new URLSearchParams(new FormData(form)).toString();
-    history.replaceState({}, "", url);
-  });
+document.addEventListener("htmx:afterRequest", (event) => {
+  if(!(event.target instanceof HTMLFormElement)) return;
+  const url = new URL(window.location.href);
+  url.search = new URLSearchParams(new FormData(event.target)).toString();
+  history.replaceState({}, "", url);
 });
