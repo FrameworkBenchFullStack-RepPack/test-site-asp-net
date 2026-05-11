f534105501095fbf229632c173e8d2d654c21270
Anton <dev@atjn.dk>
2026-05-11 13:02:19 +0200

    Fix query selector issue

--- test-site/wwwroot/js/live.js
+++ test-site/wwwroot/js/live.js
@@ -8,9 +8,9 @@ const connection = new signalR.HubConnectionBuilder()
 connection.on("ReceiveNumbers", function (data) {
     const element = document.querySelector("#live-data");
     if(!element) return;
-    element.getElementById("sig-1").innerText = data.number1;
-    element.getElementById("sig-2").innerText = data.number2;
-    element.getElementById("sig-3").innerText = data.number3;
+    element.querySelector("#sig-1").innerText = data.number1;
+    element.querySelector("#sig-2").innerText = data.number2;
+    element.querySelector("#sig-3").innerText = data.number3;
 });
 
 function startSignalR() {
